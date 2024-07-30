d4105153ef1ef26a4325c5c32a85af81b8a52d49
bre.schoun <breawn.schoun@childrenscolorado.org>
2024-07-30 14:55:56 -0600

    Moved AHRS into puck-input.js

--- puck/puck-arcade.js
+++ puck/puck-arcade.js
@@ -2,23 +2,22 @@
 var footer = document.getElementById("footer");
 footer.innerHTML = "<button id=\"btnConnect\">Connect Puck.js</button>" + footer.innerHTML;
 
-var script1 = document.createElement('script');
-script1.type = 'text/javascript';
-script1.src = 'https://www.puck-js.com/puck.js';
-document.body.appendChild(script1);
+var puckJsScript = document.createElement('script');
+puckJsScript.type = 'text/javascript';
+puckJsScript.src = 'https://www.puck-js.com/puck.js';
+document.body.appendChild(puckJsScript);
 
-var script2 = document.createElement('script');
-script2.type = 'text/javascript';
-script2.src = 'puck/www-ahrs.js';
-document.body.appendChild(script2);
+var arcadeInputScript = document.createElement('script');
+arcadeInputScript.type = 'text/javascript';
+arcadeInputScript.src = 'puck/arcade-input.js';
+document.body.appendChild(arcadeInputScript);
 
-var script3 = document.createElement('script');
-script3.type = 'text/javascript';
-script3.src = 'puck/arcade-input.js';
-document.body.appendChild(script3);
+var puckInputScript = document.createElement('script');
+puckInputScript.type = 'text/javascript';
+puckInputScript.src = 'puck/puck-input.js';
+document.body.appendChild(puckInputScript);
 
-var script4 = document.createElement('script');
-script4.type = 'text/javascript';
-script4.src = 'puck/puck-input.js';
-document.body.appendChild(script4);
-
+var puckCss = document.createElement('link');
+puckCss.rel = 'stylesheet';
+puckCss.href = 'puck/puck-arcade.css';
+document.body.appendChild(puckCss);
